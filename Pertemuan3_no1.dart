37dc917e084181cc91eb4fb744dae660e6c1b081
nuryasin36 <67747582+nuryasin36@users.noreply.github.com>
2023-02-12 22:38:44 +0700

    Update Pertemuan3_no1.dart
    Menambah keterangan

--- Pertemuan3_no1.dart
+++ Pertemuan3_no1.dart
@@ -1,13 +1,16 @@
-import 'package:no1/no1.dart' as no1;
+/// Library untuk menampilkan input
 import 'dart:io';
 
 void main() {
+  /// Menampilkan hasil
   print("Nilai Anda adalah ${hitungNilai()}");
 }
 
 String hitungNilai() {
+  /// Input
   int? nilai = int.parse(stdin.readLineSync()!);
 
+  /// Function untuk mengetahui nilai
   if (nilai > 70 && nilai <= 100) {
     return "A";
   } else if (nilai < 70 && nilai > 40) {
